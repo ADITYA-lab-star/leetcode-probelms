ff2f4448e13c5f5df1ce0e1deeaa0beea1ccb6bf
ADITYA SAI <adityasaikoka@gmail.com>
2025-07-05 15:22:29 +0530

    Add files via upload

---
+++ L3024.java
@@ -0,0 +1,17 @@
+public class L3024 {
+    public String triangleType(int[] nums) {
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+        if ((a + b > c) && (b + c > a) && (c + a > b)) {
+            if (a == b && b == c && c == a) {
+                return "equilateral";
+            } else if (a == b || b == c || c == a) {
+                return "isosceles";
+            } else {
+                return "scalene";
+            }
+        }
+        return "none";
+    }
+}
